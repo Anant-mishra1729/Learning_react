d9c20521cea22577cf7c0c9d59ecf53ac8f9af3f
anant <amishra1577@gmail.com>
2021-08-31 21:52:42 +0530

    Using hooks and states

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,25 +1,45 @@
 import React, { useState } from 'react'
 
+// Usestate -> Hooks => For handling states in functional based components
+
+
 
 export default function Textform() {
 
+    // Convert to uppercase
     const handleUpClick = () => {
         setUpper(text.toUpperCase());
     }
 
+    // Convert to lowercase
+    const handleLowClick = () => {
+        setUpper(text.toLowerCase());
+    }
+
+    // Without this you can't write in text area
     const handleOnChange = (event) => {
         setUpper(event.target.value);
     }
 
-    // States
+    // Using hook
     const [text, setUpper] = useState('');
 
     return (
-        <div>
-            <div className="mb-3">
-                <textarea className="form-control" value={text} placeholder="Enter your text here..." onChange={handleOnChange} id="myText" rows="8"></textarea>
+        <>
+            <div>
+                <div className="mb-3">
+                    <textarea className="form-control" value={text} placeholder="Enter your text here..." onChange={handleOnChange} id="myText" rows="8"></textarea>
+                </div>
+                <button className="btn btn-primary mx-1" id="toUpper" onClick={handleUpClick}>Convert to uppercase</button>
+                <button className="btn btn-primary mx-1" id="toUpper" onClick={handleLowClick}>Convert to lowercase</button>
             </div>
-            <button className="btn btn-primary" id="toUpper" onClick={handleUpClick}>Convert to uppercase</button>
-        </div>
+
+            <div className="container my-2">
+                <h1>Summary</h1>
+                <p>{text.split(" ").length} words and {text.length} characters</p>
+                <p>Reading time : <span>{text.split(' ').length * 0.008}</span></p>
+            </div>
+        </>
     )
 }
+
