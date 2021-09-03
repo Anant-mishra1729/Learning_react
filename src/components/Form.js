a1f3e21323668e8014a2e0947de05cafce52354d
Anant <amishra1577@gmail.com>
2021-09-03 21:03:44 +0530

    Slight changes

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,45 +1,64 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 // Usestate -> Hooks => For handling states in functional based components
 
-
-
 export default function Textform() {
+  // Convert to uppercase
+  const handleUpClick = () => {
+    setUpper(text.toUpperCase());
+  };
 
-    // Convert to uppercase
-    const handleUpClick = () => {
-        setUpper(text.toUpperCase());
-    }
+  // Convert to lowercase
+  const handleLowClick = () => {
+    setUpper(text.toLowerCase());
+  };
 
-    // Convert to lowercase
-    const handleLowClick = () => {
-        setUpper(text.toLowerCase());
-    }
+  // Without this you can't write in text area
+  const handleOnChange = (event) => {
+    setUpper(event.target.value);
+  };
 
-    // Without this you can't write in text area
-    const handleOnChange = (event) => {
-        setUpper(event.target.value);
-    }
+  // Using hook
+  const [text, setUpper] = useState("");
 
-    // Using hook
-    const [text, setUpper] = useState('');
+  return (
+    <>
+      <div>
+        <div className="mb-3">
+          <textarea
+            className="form-control"
+            value={text}
+            placeholder="Enter your text here..."
+            onChange={handleOnChange}
+            id="myText"
+            rows="8"
+          ></textarea>
+        </div>
+        <button
+          className="btn btn-primary mx-1"
+          id="toUpper"
+          onClick={handleUpClick}
+        >
+          Convert to uppercase
+        </button>
+        <button
+          className="btn btn-primary mx-1"
+          id="toUpper"
+          onClick={handleLowClick}
+        >
+          Convert to lowercase
+        </button>
+      </div>
 
-    return (
-        <>
-            <div>
-                <div className="mb-3">
-                    <textarea className="form-control" value={text} placeholder="Enter your text here..." onChange={handleOnChange} id="myText" rows="8"></textarea>
-                </div>
-                <button className="btn btn-primary mx-1" id="toUpper" onClick={handleUpClick}>Convert to uppercase</button>
-                <button className="btn btn-primary mx-1" id="toUpper" onClick={handleLowClick}>Convert to lowercase</button>
-            </div>
-
-            <div className="container my-2">
-                <h1>Summary</h1>
-                <p>{text.split(" ").length} words and {text.length} characters</p>
-                <p>Reading time : <span>{text.split(' ').length * 0.008}</span></p>
-            </div>
-        </>
-    )
+      <div className="container my-2">
+        <h1>Summary</h1>
+        <p>
+          {text.split(" ").length} words and {text.length} characters
+        </p>
+        <p>
+          Reading time : <span>{text.split(" ").length * 0.008}</span>
+        </p>
+      </div>
+    </>
+  );
 }
-
