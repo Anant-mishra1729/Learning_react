3e6edd83243295348335e336f4b2cf43c27b6d37
Anant <amishra1577@gmail.com>
2021-09-05 22:10:15 +0530

    Added icons

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -30,55 +30,8 @@ export default function Navbar(props) {
                   About us
                 </a>
               </li>
-              <li>
-                <a className="nav-link active" aria-current="page" href="/">
-                  Contact
-                </a>
-              </li>
-              <li className="nav-item dropdown">
-                <a
-                  className="nav-link dropdown-toggle"
-                  href="/"
-                  id="navbarDropdown"
-                  role="button"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  Services
-                </a>
-                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                  <li>
-                    <a className="dropdown-item" href="/">
-                      Action
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="/">
-                      Another action
-                    </a>
-                  </li>
-                  <li>
-                    <hr className="dropdown-divider" />
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="/">
-                      Something else here
-                    </a>
-                  </li>
-                </ul>
-              </li>
+              
             </ul>
-            {/* <form className="d-flex">
-              <input
-                className="form-control me-2"
-                type="search"
-                placeholder="Search"
-                aria-label="Search"
-              />
-              <button className="btn btn-outline-success" type="submit">
-                Search
-              </button>
-            </form> */}
             <div className={`form-check form-switch text-${props.mode ==='light'?'dark':'light'}`}>
               <input
                 className="form-check-input"
