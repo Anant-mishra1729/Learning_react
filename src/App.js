b84b098cb4a45d395427fdadbf385641c2970efe
Anant Mishra <84588156+Anant-mishra1729@users.noreply.github.com>
2021-09-12 20:07:00 +0530

    Changed app.js

--- src/App.js
+++ src/App.js
@@ -1,68 +1,60 @@
 // import './App.css';
-import About from './components/About';
-import Textform from './components/Form';
-import Navbar from './components/Navbar';
-import React, { useState } from 'react'
-import Alert from './components/Alert';
+import About from "./components/About";
+import Textform from "./components/Form";
+import Navbar from "./components/Navbar";
+import React, { useState } from "react";
+import Alert from "./components/Alert";
 
 // Importing react router -> Used to change contents of page without reloading page
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
-  
   const toggleMode = () => {
-    if(mode === 'light')
-    {
-      setMode('dark');
+    if (mode === "light") {
+      setMode("dark");
       document.body.style.backgroundColor = "#292C35";
-      showAlert("Dark mode is enabled","success");
-    }
-    else
-    {
-      setMode('light');
+      showAlert("Dark mode is enabled", "success");
+    } else {
+      setMode("light");
       document.body.style.backgroundColor = "#fff";
-      showAlert("Light mode is enabled","success");
+      showAlert("Light mode is enabled", "success");
     }
-  }
-  
+  };
+
   const showAlert = (msg, type) => {
     setAlert({
-      message : msg,
-      type : type
+      message: msg,
+      type: type,
     });
     setTimeout(() => {
       setAlert(null);
     }, 500);
   };
 
-  const [mode, setMode] = useState('light'); // Hook for dark mode 
-  const [alert,setAlert] = useState(null);
+  const [mode, setMode] = useState("light"); // Hook for dark mode
+  const [alert, setAlert] = useState(null);
 
   return (
-    <>
-      {/* Used default about_us value from navbar.js*/}
-        <Router>
-      <Navbar title="Texter" aboutText="About us" mode = {mode} toggleMode = {toggleMode}/>
-      <Alert alert = {alert}/>
+    <Router>
+      <Navbar
+        title="Texter"
+        aboutText="About us"
+        mode={mode}
+        toggleMode={toggleMode}
+      />
+      <Alert alert={alert} />
       <div className="container my-3">
-
-        {/* exact path is used for complete matching of path url */}
-      <Switch>
+        <Switch>
           <Route exact path="/about">
             <About />
           </Route>
-      
+
           <Route exact path="/">
-          <Textform mode = {mode} showAlert = {showAlert}/>
+            <Textform mode={mode} showAlert={showAlert} />
           </Route>
         </Switch>
       </div>
-      </Router>
-    </>
+    </Router>
   );
 }
 
